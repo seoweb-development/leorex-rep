cec4a2efba10d44a48150d35e9a44adf40f80e9e
Gerry <gerry@seoweb.co.il>
2017-10-02 15:57:38 +0300

    folder and files of tamplate parts

--- wp-content/themes/leorex_seoweb/front-page.php
+++ wp-content/themes/leorex_seoweb/front-page.php
@@ -9,7 +9,7 @@ get_header(); ?>
 
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
-
+            <?php require_once('./wp-content/themes/leorex_seoweb/tamplate-parts/product-slider.php') ?>
             <?php if ( have_posts() ) : ?>
 
                 <?php
@@ -27,27 +27,8 @@ get_header(); ?>
 
             endif;
             ?>
-<div class="before_after_container">
-    <div class="seven_minutes_before">
-        before using <br> booster gold
-    </div>
-    <div class="after_image">
-        <div class="seven_minutes_after">
-            7 minutes <br> after...
-        </div>
-<img src="../wp-content/themes/leorex_seoweb/images/after-1920.jpg">
-    </div>
-    <div class="before_image_container">
-        <div class="before_image">
 
-            <div class="slider_button">
-            </div>
-            <img src="../wp-content/themes/leorex_seoweb/images/before-1920.jpg"" alt="">
-        </div>
-        <div class="slider_button">
-        </div>
-    </div>
-</div>
+            <?php require_once('./wp-content/themes/leorex_seoweb/tamplate-parts/beforeafter.php') ?>
         </main><!-- .site-main -->
     </div><!-- .content-area -->
 
---
+++ wp-content/themes/leorex_seoweb/tamplate-parts/beforeafter.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<div class="before_after_container">
+    <div class="seven_minutes_before">
+        before using <br> booster gold
+    </div>
+    <div class="after_image">
+        <div class="seven_minutes_after">
+            7 minutes <br> after...
+        </div>
+        <img src="/wp-content/themes/leorex_seoweb/images/after-1920.jpg">
+    </div>
+    <div class="before_image_container">
+        <div class="before_image">
+
+            <div class="slider_button">
+            </div>
+            <img src="/wp-content/themes/leorex_seoweb/images/before-1920.jpg"" alt="">
+        </div>
+        <div class="slider_button">
+        </div>
+    </div>
+</div>
+
