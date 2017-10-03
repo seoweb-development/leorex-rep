c55b54e45f50b373738ffd4c60b75d42df10409b
Gerry <gerry@seoweb.co.il>
2017-10-03 10:44:42 +0300

    xxx

--- wp-content/themes/leorex_seoweb/front-page.php
+++ wp-content/themes/leorex_seoweb/front-page.php
@@ -10,24 +10,25 @@ get_header(); ?>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
             <?php require_once('./wp-content/themes/leorex_seoweb/tamplate-parts/product-slider.php') ?>
-            <?php if ( have_posts() ) : ?>
+            <div style="margin:100px 0; padding: 0 25px;">
+                <?php if ( have_posts() ) : ?>
 
-                <?php
-                // Start the loop.
-                while ( have_posts() ) : the_post();
-
-                    ?><h1><?php the_title() ?></h1>
-                    <div><?php the_content() ?></div>
                     <?php
-                endwhile;
+                    // Start the loop.
+                    while ( have_posts() ) : the_post();
+
+                        ?><h1><?php the_title() ?></h1>
+                        <div><?php the_content() ?></div>
+                        <?php
+                    endwhile;
 
 
-            else :
+                else :
 
 
-            endif;
-            ?>
-
+                endif;
+                ?>
+            </div>
             <?php require_once('./wp-content/themes/leorex_seoweb/tamplate-parts/beforeafter.php') ?>
         </main><!-- .site-main -->
     </div><!-- .content-area -->
--- wp-content/themes/leorex_seoweb/tamplate-parts/beforeafter.php
+++ wp-content/themes/leorex_seoweb/tamplate-parts/beforeafter.php
@@ -2,25 +2,44 @@
 
 ?>
 
-<div class="before_after_container">
-    <div class="seven_minutes_before">
-        before using <br> booster gold
-    </div>
+<div class="before_after_container desktop_ba">
     <div class="after_image">
-        <div class="seven_minutes_after">
+        <div class="seven_minutes_after before-after-text">
             7 minutes <br> after...
         </div>
         <img src="/wp-content/themes/leorex_seoweb/images/after-1920.jpg">
     </div>
     <div class="before_image_container">
         <div class="before_image">
-
-            <div class="slider_button">
+            <div class="before-after-text-wrap">
+                <div class="seven_minutes_before before-after-text">
+                    before using <br> booster gold
+                </div>
             </div>
+            <div class="slider_button"></div>
             <img src="/wp-content/themes/leorex_seoweb/images/before-1920.jpg"" alt="">
         </div>
-        <div class="slider_button">
+        <div class="slider_button"></div>
+    </div>
+</div>
+<div class="before_after_container mobile_ba">
+    <div class="after_image">
+        <div class="seven_minutes_after before-after-text">
+            7 minutes <br> after...
         </div>
+        <img src="/wp-content/themes/leorex_seoweb/images/ba_after.jpg">
+    </div>
+    <div class="before_image_container">
+        <div class="before_image">
+            <div class="before-after-text-wrap">
+                <div class="seven_minutes_before before-after-text">
+                    before using <br> booster gold
+                </div>
+            </div>
+            <div class="slider_button"></div>
+            <img src="/wp-content/themes/leorex_seoweb/images/ba_before.jpg"" alt="">
+        </div>
+        <div class="slider_button"></div>
     </div>
 </div>
 
