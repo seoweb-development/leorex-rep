b760404d97fe7bfac2d6a1c105b62a1015e022b3
genady <genadi@seoweb.co.il>
2017-10-08 19:06:25 +0300

    advantages component decktop

--- wp-content/themes/leorex_seoweb/tamplate-parts/advantages.php
+++ wp-content/themes/leorex_seoweb/tamplate-parts/advantages.php
@@ -1,15 +1,29 @@
 <div class="advantages_container">
-    <img src="/wp-content/themes/leorex_seoweb/images/advantages-1920px.jpg" alt="" class="advantages_backdround_image">
+    <img src="/wp-content/themes/leorex_seoweb/images/advantages-1920px.jpg" alt="" class="advantages_backdround_image advantages_decktop_image">
+    <img src="/wp-content/themes/leorex_seoweb/images/advantages-320px.jpg" alt="" class="advantages_backdround_image advantages_mobile_image">
     <div class="advantages_info_container">
         <div class="advantages_info_top">
             <div class="advantages_title">Lorem ipsum</div>
-            <div class="advantages_description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-            <div class="advantages_special_offers">special offers</div>
+            <div class="advantages_description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+            <div class="advantages_special_offers advantages_special_offers_deck ">special offers</div>
         </div>
         <div class="advantages_info_body">
-            <div class="offer_link"><div class="link_icon"><div class="link_title">Lorem ipsum dolor</div></div></div>
-            <div class="offer_link"><div class="link_icon"><div class="link_title">Lorem ipsum dolor</div></div></div>
-            <div class="offer_link"><div class="link_icon"><div class="link_title">Lorem ipsum dolor</div></div></div>
+            <div class="offer_link">
+                <div class="link_icon link_icon1"></div>
+                <div class="link_title">Lorem ipsum dolor</div>
+            </div>
+            <div class="offer_link">
+                <div class="link_icon link_icon2"></div>
+                <div class="link_title">Lorem ipsum dolor</div>
+
+            </div>
+            <div class="offer_link">
+                <div class="link_icon link_icon3"></div>
+                <div class="link_title">Lorem ipsum dolor</div>
+            </div>
+            <div class="advantages_special_offers advantages_special_offers_mobile ">special offers</div>
         </div>
 
     </div>
