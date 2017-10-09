ee51a68091eeeedb00273f6cf78951ebde7b2a8b
genady <genadi@seoweb.co.il>
2017-10-09 13:17:21 +0300

    advantages mobile UI

--- wp-content/themes/leorex_seoweb/tamplate-parts/advantages.php
+++ wp-content/themes/leorex_seoweb/tamplate-parts/advantages.php
@@ -1,4 +1,4 @@
-<div class="advantages_container">
+<div class="advantages_container" id="advantages">
     <img src="/wp-content/themes/leorex_seoweb/images/advantages-1920px.jpg" alt="" class="advantages_backdround_image advantages_decktop_image">
     <img src="/wp-content/themes/leorex_seoweb/images/advantages-320px.jpg" alt="" class="advantages_backdround_image advantages_mobile_image">
     <div class="advantages_info_container">
@@ -23,8 +23,9 @@
                 <div class="link_icon link_icon3"></div>
                 <div class="link_title">Lorem ipsum dolor</div>
             </div>
-            <div class="advantages_special_offers advantages_special_offers_mobile ">special offers</div>
+
         </div>
+        <div class="advantages_special_offers advantages_special_offers_mobile ">special offers</div>
 
     </div>
 </div>
